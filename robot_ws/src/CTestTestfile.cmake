d9ebe57f64d28252c99da9826825a8313937258c
tylerjulian <tyler.james.julian@ou.edu>
2020-09-23 19:09:49 -0500

    finished the control_node

---
+++ robot_ws/src/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/tyler/Cs4023/robot_ws/src
+# Build directory: /home/tyler/Cs4023/robot_ws/src
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(project1)
---
+++ robot_ws/src/project1/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/tyler/Cs4023/robot_ws/src/project1
+# Build directory: /home/tyler/Cs4023/robot_ws/src/project1
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
