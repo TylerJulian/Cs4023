718394d65b2f7aa1a714c76ba2dd0bf8c228bb34
yasha <yashar@ubuntu.ubuntu-domain>
2020-09-25 02:05:54 -0500

    fixed isseus: initial min_distance is the sensor range_max, changed to avoidance to 0.5548 meter to consider the distance from sensor to front bumper

--- robot_ws/src/project1/src/laser_work.py
+++ robot_ws/src/project1/src/laser_work.py
@@ -15,12 +15,12 @@ class laser_work:
     CENTER = 'C'
 
 
-    # 1 foot tolerance in meter 
-    AVOIDANCE_DISTANCE = 0.3048
+    # 1 foot tolerance in meter + distance of sensor from front bumper (0.25 m)
+    AVOIDANCE_DISTANCE = 0.5548
 
     # symmetric, asymmetric determination tolerance in angle 
     # Used to determine if the obstacle is symmetric or 
-    ANGLE_TOLERANCE = 5.0
+    ANGLE_TOLERANCE = 8.0
 
 
     @staticmethod
@@ -46,6 +46,17 @@ class laser_work:
         meaning that the laser rotates and sample readings counterclockwise, right to 
         left
         '''
+
+        if laser_info is None:
+            raise AttributeError()
+
+        # max laser readable distance
+        max_distance = laser_info.range_max
+
+        # min laser readable distance
+        min_distance = laser_info.range_min
+        
+
         # minimum reading angle, the right side of the robot
         min_angle = math.degrees(laser_info.angle_min)
 
@@ -62,19 +73,20 @@ class laser_work:
         total_reading_count = len(ranges)
 
         # middle or supposedly angle zero reading index
-        middle_reading_index = math.floor(total_reading_count/2)
+        middle_reading_index = int(math.floor(total_reading_count/2))
 
         # scan left and right sides 
-        right_max_angle, min_right_distance = laser_work.scan_right_side(ranges,middle_reading_index,increment)
-        left_max_angle, min_left_distance = laser_work.scan_left_side(ranges,middle_reading_index,increment)
+        right_max_angle, min_right_distance = laser_work.scan_right_side(ranges,middle_reading_index,increment, max_distance)
+        left_max_angle, min_left_distance = laser_work.scan_left_side(ranges,middle_reading_index,increment, max_distance)
         
         # find the minimum distance
         min_distance = min([min_left_distance,min_right_distance])
+        # print("Min Distance: " + str(min_distance))
 
         # check if there is any obstacles in defined distance
         obstacle_exists = False
         if min_distance > laser_work.AVOIDANCE_DISTANCE:
-            # No obstacles, just return 
+            # No obstacles, just return float('inf')
             return False, None, None, None ### CONDITION 1 EXIT : No obstacles
 
         # find angle difference
@@ -84,7 +96,7 @@ class laser_work:
         if abs(angle_delta) < laser_work.ANGLE_TOLERANCE:
             obstacle_direction = laser_work.CENTER
         else:
-            if angle_delta > 0:
+            if angle_delta < 0:
                 # obstacle mainly is in left side
                 obstacle_direction = laser_work.LEFT
             
@@ -103,9 +115,9 @@ class laser_work:
 
 
     @staticmethod
-    def scan_right_side(ranges, mri, increment):
+    def scan_right_side(ranges, mri, increment, max_dist):
         right_side_angle = 0
-        min_distance = float('inf')
+        min_distance = max_dist
         for i in range(mri, -1, -1):
             if ranges[i] < laser_work.AVOIDANCE_DISTANCE:
                 current_angle = (mri - i) * increment
@@ -114,15 +126,17 @@ class laser_work:
                     right_side_angle = current_angle
                 # update min_distance
                 if ranges[i] < min_distance:
+                    
                     min_distance = ranges[i]
         
         return right_side_angle, min_distance
     
     @staticmethod
-    def scan_left_side(ranges ,mri, increment):
+    def scan_left_side(ranges ,mri, increment, max_dist):
         left_side_angle = 0
-        min_distance = float('inf')
+        min_distance = max_dist
         for i in range(mri, len(ranges)):
+            
             if ranges[i] < laser_work.AVOIDANCE_DISTANCE:
                 current_angle = (i - mri) * increment
                 # update max angle
@@ -131,6 +145,7 @@ class laser_work:
                 # update min_distance
                 if ranges[i] < min_distance:
                     min_distance = ranges[i]
+                    
         
         return left_side_angle, min_distance
 
