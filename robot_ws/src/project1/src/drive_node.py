dcde97d0e01d443400c217d8e4e26cc8127b3657
yasharAhari <yasharahari@ou.edu>
2020-09-26 00:31:08 -0500

    added the ros paramters for HALT, WAIT and KEY

--- robot_ws/src/project1/src/drive_node.py
+++ robot_ws/src/project1/src/drive_node.py
@@ -17,21 +17,31 @@ path_plan_command = Twist()
 def get_keyboard_command(key_msg):
 	global keyboard_command
 	keyboard_command = key_msg
-	#print(keyboard_command)
+	# force other nodes to stop moving and wait when there is a keyboard
+	if not is_twist_empty(keyboard_command):
+		rospy.set_param("KEY", True)
+		
+	# print(keyboard_command)
 
 def bumper_state(bumper_state):
 	global is_bumped
 	is_bumped = bumper_state.state == 1 or is_bumped
-	print(is_bumped)
+	# print(is_bumped)
 
 def publish_command(timer_event):
 	#This will be called if the bumper is pressed down
 	if is_bumped == 1:
+		# Set a Halt parameter to true so other nodes stop trying to move 
+		rospy.set_param("HALT", True)
+
 		stop_movement_command = Twist()
 		stop_movement_command.linear = Vector3(0,0,0)
 		stop_movement_command.angular = Vector3(0,0,0)
 		pub.publish(stop_movement_command)
-		print("bumped")
+
+		
+
+		#print("bumped")
 		return
 
 	#Checks to see if there is a keyboard command or not. If there is no keyboard command then the path_planning_command is published
@@ -39,6 +49,9 @@ def publish_command(timer_event):
 		pub.publish(path_plan_command)
 	else:
 		pub.publish(keyboard_command)
+		rospy.set_param("KEY", False)
+		# clear the key wait restriction
+		
 
 #check if the twist message is zero
 def is_twist_empty(keyboard_command):
@@ -54,12 +67,21 @@ def main():
 	rospy.init_node("drive_node", anonymous=True)
 	#global publisher
 	pub = rospy.Publisher("/mobile_base/commands/velocity",Twist, queue_size=1)
-	
+
 	#subscribe to keyboard
 	rospy.Subscriber("/project1/keyboard_input", Twist, get_keyboard_command)
 	#subscribe to bumper
 	rospy.Subscriber("/mobile_base/events/bumper", BumperEvent, bumper_state)
-	
+
+	# Create a halt parameter, this will be used by other nodes to stop when true
+	rospy.set_param("HALT", False)
+
+	# Create a wait parameter, this will used to negotiate movement between nodes
+	rospy.set_param("WAIT", False)
+
+	# Create a KEY parameter which specifies that there is a keyboard entry, force other nodes stop
+	rospy.set_param("KEY", False)
+
 	#send command 5 times every second
 	rospy.Timer(rospy.Duration(secs = .2), publish_command)
 	#do it again
