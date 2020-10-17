3ec4bea228dcd4128cdd36b9c266f2cc6e553cb6
yasharAhari <yasharahari@ou.edu>
2020-10-16 22:39:15 -0500

    fixed some issues, added comments

--- robot_ws/src/project2/src/planning/task_planner.py
+++ robot_ws/src/project2/src/planning/task_planner.py
@@ -4,21 +4,27 @@ from user_interface.plan_input import PlanInput
 
 class TaskPlanner:
 
-    PLAN_EXIST = False
-    TASK_LIST = None
-    PLAN_LIST = None
-    curr_location = (3.281, 2.5) # default position. Gets immediately replaced by the actual position
+    PLAN_EXIST = False  # True when there is a plan
+    TASK_LIST = None  # List of points which robot have to reach
+    PLAN_LIST = None  # List of the plans
+    curr_location = (3.281, 2.5)  # default position. Gets immediately replaced by the actual position
+    TASK_QUEUE = []  # A queue which is same as the task list, however the top will pop on every enquire
 
-
-    def __init__(self,location, dispatcher):
+    def __init__(self, dispatcher):
         self.dispatcher = dispatcher
-        self.timer = rospy.Timer(rospy.Duration(secs=1), self.control())
+        # self.timer = rospy.Timer(rospy.Duration(secs=1), self.control)
         self.curr_location = self.dispatcher.get_current_location
-    def control(self):
+        self.ask_user_for_plan()
+
+    def control(self, timer_event):
         if not self.PLAN_EXIST:
             self.ask_user_for_plan()
 
     def ask_user_for_plan(self):
+        """
+        The user prompt
+        @return: None
+        """
         while True:
             u_in = PlanInput.initial_user_prompt()
             if u_in == 1:
@@ -33,24 +39,43 @@ class TaskPlanner:
                 break
             else:
                 print("Wrong option")
-# this function sorts the given list by the distance of the first coordinate pairs from the starting point
-# The second point is immediately added after the firsts point.
-# This makes it easy to remove the secondary task from the list.
-# Here is what the example given points become. First line is the input list of points and the second list is the output
-#[((2, 2.5), (12.5, 2.5)), ((12.5, 12.5), (5, 12.5)), ((2, 3), (9, 8))]
-#[(2, 2.5), (12.5, 2.5), (2, 3), (9, 8), (12.5, 12.5), (5, 12.5)]
 
     def create_new_plan(self):
+        """
+        this function sorts the given list by the distance of the first coordinate pairs from the starting point
+        The second point is immediately added after the firsts point.
+        This makes it easy to remove the secondary task from the list.
+        Here is what the example given points become. First line is the input list of points and the second list is the output
+        [((2, 2.5), (12.5, 2.5)), ((12.5, 12.5), (5, 12.5)), ((2, 3), (9, 8))]
+        [(2, 2.5), (12.5, 2.5), (2, 3), (9, 8), (12.5, 12.5), (5, 12.5)]
+        @return None
+        """
         print(self.TASK_LIST)
-        self.TASK_LIST.sort(key = self.distance)
+        self.TASK_LIST.sort(key=self.distance)
         temp_list = []
         for x in self.TASK_LIST:
             temp_list.append(x[0])
             temp_list.append(x[1])
         self.TASK_LIST = temp_list
+        self.TASK_QUEUE = self.TASK_LIST
         print(self.TASK_LIST)
 
     def distance(self, pos):
+        """
+        Calculates the distance from current location to the given position
+        @param pos: given position
+        @return: distance: float
+        """
         #print(pos)
         #print(self.curr_location[0])
-        return (pos[0][0]-self.curr_location[0])**2 + (pos[0][1] - self.curr_location[1])**2
+        # the location information is ((x,y),angle)
+        # so x => location[0][0]
+        # and y = location[0][1]
+        return (pos[0][0]-self.curr_location()[0][1])**2 + (pos[0][1] - self.curr_location()[0][1])**2
+
+    def get_next_task(self):
+        """
+        Pops the next task from queue and returns
+        @return: (x,y) next task: tuple
+        """
+        return self.TASK_QUEUE.pop(0)
