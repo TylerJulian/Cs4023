ff72170d6bb2a443c49480eb7ef57f21c62b71cc
yasharAhari <yasharahari@ou.edu>
2020-10-17 16:16:55 -0500

    WIP Navigation: Still trying to make it work, added a new way to check if the point meet. Dome TODO LEFT

--- robot_ws/src/project2/src/navigation/navigation.py
+++ robot_ws/src/project2/src/navigation/navigation.py
@@ -6,8 +6,14 @@ from movement.movement_commands import MovementCommands
 
 
 class Navigation:
+
+    # distance to a goal in which robot calls it success
+    SUCCESS_DISTANCE = 1
+    DEBUG = True
+
     def __init__(self, dispatcher):
         self.__dispatcher = dispatcher
+        task_stack = []
         while dispatcher.task_planner.is_next_task_exists():
             # Always check out on movement status
             if rospy.get_param("HALT"):
@@ -16,14 +22,36 @@ class Navigation:
             while rospy.get_param("WAIT"):
                 # Wait while the WAIT resolves
                 pass
-            # TODO: Observe avoidance interferences
-            self.move_to_next_point()
 
-    def move_to_next_point(self):
+            # Get next point
+            if len(task_stack) == 0:
+                # If the tasks stack is empty, get one from the planner
+                next_task = self.__dispatcher.task_planner.get_next_task()
+                task_stack.append(next_task)
+                print("new task acquired: " + str(next_task))
+
+            current_coordinates = self.__dispatcher.get_current_location()[0]
+            if self.__calculate_distance(current_coordinates, task_stack[len(task_stack)-1]) <= Navigation.SUCCESS_DISTANCE:
+                print("Success reaching the point")
+                task_stack.pop(len(task_stack)-1)
+            else:
+                # TODO: Observe avoidance interferences
+                # Amend Avoidance to stop trying to turn the robot, rather notify the navigation the angle and distance
+                # of the obstacle
+                self.move_to_the_point(task_stack[len(task_stack)-1])
+                print(self.__dispatcher.get_current_location()[0])
+
+
+
+
+
+
+
+    def move_to_the_point(self, point):
         current_location_info = self.__dispatcher.get_current_location()
         current_coordinates = current_location_info[0]
         current_angle = current_location_info[1]
-        goal = self.__dispatcher.task_planner.get_next_task()
+        goal = point
 
         # At this point, we need two things:
         # 1- Distance
@@ -31,6 +59,7 @@ class Navigation:
 
         # 2- Angle
         g_angle = math.degrees(math.atan2(goal[1]-current_coordinates[1], goal[0]-current_coordinates[1]))
+
 
         # find out which side would be closer to turn
         clockwise = False
@@ -40,14 +69,19 @@ class Navigation:
             angle = g_angle + (360 - current_angle)
             clockwise = True
 
+        if Navigation.DEBUG:
+            print("Next Move: ")
+            print("Angle Between points: " + str(g_angle))
+            print("Robot Angle change: " + str(angle))
+            print("Distance: " + str(distance))
+
         # turn the robot
         MovementCommands.turn_robot(angle, clockwise)
 
         # move that robot
+        # TODO: DAMN! I forgot that move robot works in meter!
         MovementCommands.move_robot(distance)
 
-
-        print("Angle: " + str(angle))
 
 
     @staticmethod
