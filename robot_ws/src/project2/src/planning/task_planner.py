10cb89910e218ed6ecf31540cd586bec0cff2b3b
yasharAhari <yasharahari@ou.edu>
2020-10-18 02:47:57 -0500

    Changed the planning scheme totaly

--- robot_ws/src/project2/src/planning/task_planner.py
+++ robot_ws/src/project2/src/planning/task_planner.py
@@ -1,12 +1,15 @@
+import math
+
 import rospy
 
 from user_interface.plan_input import PlanInput
+from journey.journey import Journey, JourneyBook
 
 class TaskPlanner:
 
     PLAN_EXIST = False  # True when there is a plan
-    TASK_LIST = None  # List of points which robot have to reach
-    PLAN_LIST = None  # List of the plans
+    TASK_LIST = []  # List of points which robot have to reach
+    PLANED_LIST = []  # List of the plans
     curr_location = (3.281, 2.5)  # default position. Gets immediately replaced by the actual position
     TASK_QUEUE = []  # A queue which is same as the task list, however the top will pop on every enquire
 
@@ -42,23 +45,102 @@ class TaskPlanner:
 
     def create_new_plan(self):
         """
-        this function sorts the given list by the distance of the first coordinate pairs from the starting point
-        The second point is immediately added after the firsts point.
-        This makes it easy to remove the secondary task from the list.
-        Here is what the example given points become. First line is the input list of points and the second list is the output
-        [((2, 2.5), (12.5, 2.5)), ((12.5, 12.5), (5, 12.5)), ((2, 3), (9, 8))]
-        [(2, 2.5), (12.5, 2.5), (2, 3), (9, 8), (12.5, 12.5), (5, 12.5)]
+        Creates a plan from the tasks.
+        let have three tasks
+        1 - (x1,y1) -> (x2, y2)
+        2- (a1, b1) -> (a2, b2)
+        3- (m1, n1) -> (m2, n2)
+
+        The plan will be to move the closest starting point first, then either move to another closest starting point,
+        or an ending point which went to it's starting point before. All following the closest path first.
         @return None
         """
-        print(self.TASK_LIST)
-        self.TASK_LIST.sort(key=self.distance)
-        temp_list = []
-        for x in self.TASK_LIST:
-            temp_list.append(x[0])
-            temp_list.append(x[1])
-        self.TASK_LIST = temp_list
-        self.TASK_QUEUE = self.TASK_LIST
-        print(self.TASK_LIST)
+
+        starting_points_group = list()
+        ending_points_group = list()
+        consideration_group = list()
+        comparing_point = self.dispatcher.get_current_location()[0]
+
+        for t in self.TASK_LIST:
+            starting_points_group.append(t[0])
+            ending_points_group.append(t[1])
+
+        while len(starting_points_group) > 0 or len(consideration_group) > 0:
+
+            if len(starting_points_group) == 0:
+                # Case all group a is depleted meaning we only left with ending points
+                i = TaskPlanner.get_min_distance_index(comparing_point, consideration_group)
+                point = consideration_group.pop(i)
+                self.PLANED_LIST.append(point)
+                comparing_point = point
+
+            else:
+                i_1 = TaskPlanner.get_min_distance_index(comparing_point, starting_points_group)
+                i_2 = TaskPlanner.get_min_distance_index(comparing_point, consideration_group)
+
+                dist_1 = TaskPlanner.distance2(comparing_point, starting_points_group[i_1])
+                dist_2 = 1000
+                if i_2 >= 0:
+                    dist_2 = TaskPlanner.distance2(comparing_point, consideration_group[i_2])
+
+                if i_2 < 0 or dist_1 <= dist_2:
+
+                    # take out one from starting points
+                    start_point = starting_points_group.pop(i_1)
+                    end_point = ending_points_group.pop(i_1)
+                    print(start_point)
+                    print(end_point)
+                    consideration_group.append(end_point)
+                    self.PLANED_LIST.append(start_point)
+                    comparing_point = start_point
+
+                else:
+                    point = consideration_group.pop(i_2)
+                    self.PLANED_LIST.append(point)
+                    comparing_point = point
+
+        self.TASK_QUEUE = self.PLANED_LIST
+        self.PLAN_EXIST = True
+        print(self.PLANED_LIST)
+
+
+
+
+
+
+    @staticmethod
+    def get_min_distance_index(point, p_list):
+        """
+        gets the index of closes point in p_list to given point
+        @param point: the compare point
+        @param p_list: list of points
+        @return: index of closest point
+        """
+        closest_index = -1
+
+        if len(p_list) == 0:
+            return -1
+        closest_distance = 1000.0
+        for index in range(0, len(p_list)):
+            distance = TaskPlanner.distance2(p_list[index], point)
+            if distance <= closest_distance:
+                closest_index = index
+                closest_distance = distance
+
+        return closest_index
+
+
+    @staticmethod
+    def distance2(a, b):
+        """
+        gets the distance between two point a and b
+        @param a:
+        @param b:
+        @return: distance
+        """
+
+        return math.sqrt((b[0] - a[0])**2 + (b[1] - a[1])**2)
+
 
     def distance(self, pos):
         """
@@ -71,7 +153,7 @@ class TaskPlanner:
         # the location information is ((x,y),angle)
         # so x => location[0][0]
         # and y = location[0][1]
-        return (pos[0][0]-self.curr_location()[0][1])**2 + (pos[0][1] - self.curr_location()[0][1])**2
+        return math.sqrt((pos[0][0]-self.curr_location()[0][1])**2 + (pos[0][1] - self.curr_location()[0][1])**2)
 
     def get_next_task(self):
         """
